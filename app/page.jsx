0dab66bc5b2fca6f35aeee9f3d1da1f10b0b49d6
abdullmustyy <abdulmusty03@outlook.com>
2023-07-25 07:35:07 +0100

    Chore: Firat commit after restart

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <section className="w-full flex flex-col">
+      <h1 className="head_text text-center">Dicover & Share</h1>
+    </section>
+  );
+};
+
+export default Home;
