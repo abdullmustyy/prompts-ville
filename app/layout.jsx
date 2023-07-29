cb81a4c105500798d364d9daa2004f2bbc059a1e
abdullmustyy <abdulmusty03@outlook.com>
2023-07-29 21:10:19 +0100

    Feat: Disable text copying

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
+      <body className="select-none">
         <Provider>
           <div className="main">
             <div className="gradient" />
